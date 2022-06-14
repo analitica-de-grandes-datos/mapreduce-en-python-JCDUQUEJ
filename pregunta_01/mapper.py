6469a9cd079dc653d7b0f2e19a984536709fcb83
JCDUQUEJ <101428664+JCDUQUEJ@users.noreply.github.com>
2022-06-13 22:54:15 -0500

    V2_06/13/2022

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -2,6 +2,6 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
-
-for row in sys.stdin:
-    sys.stdout.write("{}\t1\n".format(row.split(',')[4]))
+if __name__ == '__main__':
+    for row in sys.stdin:
+        sys.stdout.write("{}\t1\n".format(row.split(',')[4]))
