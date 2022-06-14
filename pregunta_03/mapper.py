18ae07a0613212af6bad72109084fc59483e8625
JCDUQUEJ <101428664+JCDUQUEJ@users.noreply.github.com>
2022-06-13 23:41:41 -0500

    V3_06/13/2022

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,8 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    for row in sys.stdin:
+        separado = row.replace('\n','').split(',')
+        sys.stdout.write('{}\t{}\n'.format(separado[1], separado[0]))
--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,7 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    for row in sys.stdin:
+        sys.stdout.write('{}\t1\n'.format(row.split('   ')[0]))
