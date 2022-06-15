e121644a87f2558d349c707f4819dcb8950168c8
JCDUQUEJ <101428664+JCDUQUEJ@users.noreply.github.com>
2022-06-14 22:23:17 -0500

    V1.5_06/14/2022

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -2,7 +2,16 @@
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
 import sys
+from operator import itemgetter
 if __name__ == '__main__':
-    for row in sys.stdin:
+    df = sys.stdin
+    for line in df:
+        line = line.replace('\n','').split('\t')
+
+    dfsort = sorted(df,key=itemgetter(2), reverse = False)
+    dfsort = dfsort[:6]
+
+    for row in dfsort:
         row1 = row.replace('\n','').split('   ')
         sys.stdout.write('{}\t{}\t{}\n'.format(row1[0], row1[1], row1[2]))
+
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -11,21 +11,21 @@ if __name__ == '__main__':
     for line in sys.stdin:
 
         key, val1, val2 = line.replace('\n','').split("\t")
-        val1 = int(val1)
+        val1 = float(val1)
         val2 = int(val2)
 
         if key == curkey:
             total += val1
             cuenta += val2
-            promedio = total/cuenta
+            promedio = '{:1f}'.format(total/cuenta)
 
         else:
             if curkey is not None:
                 sys.stdout.write("{}\t{}\t{}\n".format(curkey, total, promedio))
 
             curkey = key
-            total = val1
-            cuenta = val2
+            total = float(val1)
+            cuenta = int(val2)
             promedio = total/cuenta
 
     sys.stdout.write("{}\t{}\t{}\n".format(curkey, total, promedio))
