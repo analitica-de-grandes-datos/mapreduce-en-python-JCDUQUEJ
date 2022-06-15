0d87416ccf4a71ee34735b2b71699a45b1c3778d
JCDUQUEJ <101428664+JCDUQUEJ@users.noreply.github.com>
2022-06-14 21:51:57 -0500

    V1.4_06/14/2022

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+from operator import itemgetter
+if __name__ == '__main__':
+    for row in sys.stdin:
+        separado = row.replace('\n','').split('\t')
+        key = int(separado[0])
+        claves = sorted(separado[1].split(','), key=itemgetter(0), reverse = False)
+        for clave in claves:
+            sys.stdout.write('{}\t{}\n'.format(clave, key))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+
+    curkey = None
+    total = ''
+
+    for line in sys.stdin:
+
+        key, val = line.replace('\n','').split("\t")
+
+        if key == curkey:
+            total +=  ',' + str(val)
+
+        else:
+            if curkey is not None:
+                sys.stdout.write("{}\t{}\n".format(curkey, total))
+
+            curkey = key
+            total = str(val)
+
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
