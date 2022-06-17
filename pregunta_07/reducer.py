f321d02ccc8a91f8514593b2ed0e31835e7c0c9e
JCDUQUEJ <101428664+JCDUQUEJ@users.noreply.github.com>
2022-06-16 20:24:39 -0500

    V1.7_06/16/2022

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,23 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+from operator import itemgetter
+
+if __name__ == '__main__':
+    tablefile = [lines.replace("\n", "").split('\t') for lines in sys.stdin]
+
+    list_dict0=[]
+    list_dict1=[]
+    list_dict2=[]
+
+    for i in tablefile:
+        list_dict0.append(i[0])
+        list_dict1.append(i[1])
+        list_dict2.append(int(i[2]))
+
+    data = list(zip(list_dict0, list_dict1, list_dict2))
+    result = sorted(data, key=itemgetter(0,2), reverse = False)
+
+    for lineas in result:
+        sys.stdout.write('{}\t{}\t{}\n'.format(lineas[0], lineas[1], lineas[2]))
